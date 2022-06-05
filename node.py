8c16f0a56e7ff764eff91574ca3c330831ba9d74
Alexei A. Smekalkine <ikle@ikle.ru>
2022-06-05 09:32:19 +0300

    node: unify variable and parameter naming, clean up

--- node.py
+++ node.py
@@ -30,22 +30,22 @@ class Name (ast.Name, Expr):
 			env[o.name] = v = te.Var ()
 			ng.add (v)
 
-	def get_type (o, env, non_generic):
+	def get_type (o, env, ng):
 		if not o.name in env:
 			raise SyntaxError ("Undefined symbol " + o.name)
 
-		return te.fresh (env[o.name], {}, non_generic)
+		return te.fresh (env[o.name], {}, ng)
 
 class Bool (ast.Bool, Expr):
 	T = te.Name ("bool")
 
-	def get_type (o, env, non_generic):
+	def get_type (o, env, ng):
 		return o.T
 
 class Int (ast.Int, Expr):
 	T = te.Name ("int")
 
-	def get_type (o, env, non_generic):
+	def get_type (o, env, ng):
 		return o.T
 
 # core compound nodes
@@ -56,20 +56,20 @@ class Pair (Expr):
 		o.y.get_free (env, ng)
 
 class Func (ast.Func, Pair):
-	def get_type (o, env, non_generic):
+	def get_type (o, env, ng):
 		new_env = env.copy ()
-		new_non_generic = non_generic.copy ()
+		new_ng  = ng.copy ()
 
-		o.x.get_free (new_env, new_non_generic)
+		o.x.get_free (new_env, new_ng)
 
-		dom = o.x.get_type (new_env, new_non_generic)
-		cod = o.y.get_type (new_env, new_non_generic)
+		dom = o.x.get_type (new_env, new_ng)
+		cod = o.y.get_type (new_env, new_ng)
 		return te.Func (dom, cod)
 
 class Apply (ast.Apply, Pair):
-	def get_type (o, env, non_generic):
-		f_type = o.x.get_type  (env, non_generic)
-		dom = o.y.get_type (env, non_generic)
+	def get_type (o, env, ng):
+		f_type = o.x.get_type (env, ng)
+		dom = o.y.get_type (env, ng)
 		cod = te.Var ()
 		te.unify (f_type, te.Func (dom, cod))
 		return cod
@@ -85,30 +85,30 @@ class Sum (ast.Sum, Pair):
 # core helper nodes
 
 class Cond (ast.Cond, Expr):
-	def get_type (o, env, non_generic):
-		c_type = o.c.get_type (env, non_generic)
+	def get_type (o, env, ng):
+		c_type = o.c.get_type (env, ng)
 		te.unify (c_type, Bool.T)
-		t_type = o.t.get_type (env, non_generic)
-		f_type = o.f.get_type (env, non_generic)
+		t_type = o.t.get_type (env, ng)
+		f_type = o.f.get_type (env, ng)
 		te.unify (t_type, f_type)
 		return t_type
 
 class Let (ast.Let, Expr):
-	def get_type (o, env, non_generic):
-		defn_type = o.defn.get_type (env, non_generic)
+	def get_type (o, env, ng):
+		defn_type = o.defn.get_type (env, ng)
 
 		new_env = {o.name: defn_type, **env}
 
-		return o.body.get_type (new_env, non_generic)
+		return o.body.get_type (new_env, ng)
 
 class Letrec (ast.Letrec, Expr):
-	def get_type (o, env, non_generic):
+	def get_type (o, env, ng):
 		new_type = te.Var ()
 
 		new_env = {o.name: new_type, **env}
-		new_non_generic = non_generic | {new_type}
+		new_ng  = ng | {new_type}
 
-		defn_type = o.defn.get_type (new_env, new_non_generic)
+		defn_type = o.defn.get_type (new_env, new_ng)
 		te.unify (new_type, defn_type)
 
-		return o.body.get_type (new_env, non_generic)
+		return o.body.get_type (new_env, ng)
